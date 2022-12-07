eaa5c65538b1e4bc9919cadc228151a7d7e595b1
mwvane <bzishvili57@gmail.com>
2022-12-07 14:01:32 +0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,32 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {
+    faCircleRight,
+    faCheckDouble,
+    faChessQueen,
+    faCircleExclamation,
+    faXmark,
+    faArrowCircleRight,
+}
+    from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+library.add(
+    faCircleRight,
+    faCheckDouble,
+    faChessQueen,
+    faCircleExclamation,
+    faXmark,
+    faArrowCircleRight,
+)
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+const app = createApp(App)
+    .use(store)
+    .use(router)
+    .use(BootstrapVue3)
+    .component("font-awesome-icon",FontAwesomeIcon)
 
-createApp(App).use(store).use(router).mount('#app')
+app.mount('#app')
